c5da0fa8a5ec41241be85237a3c70a91db24bac0
sammy00 <xiangminli@sjtu.edu.cn>
2017-12-21 13:17:18 +0800

    add a unit test for signing with bad public key

--- core/lm_test.go
+++ core/lm_test.go
@@ -23,3 +23,22 @@ func TestLMSig(t *testing.T) {
 		t.Fatal("verification failed")
 	}
 }
+
+func TestLMSigBadPk(t *testing.T) {
+	hash := sha3.Sum256([]byte("hello Leighton-Micali Signature"))
+
+	dummyOpts := new(LMOpts)
+	dummyOpts.typecode = METAOPTS_DEFAULT.typecode
+	sk, _ := GenerateKey(dummyOpts, lmotsrand.Reader)
+
+	sig, err := Sign(lmotsrand.Reader, sk, hash[:])
+	if nil != err {
+		t.Fatal(err)
+	}
+
+	sk.PublicKey.K[1] = ^sk.PublicKey.K[1]
+
+	if Verify(&sk.PublicKey, hash[:], sig) {
+		t.Fatal("verification failed")
+	}
+}
